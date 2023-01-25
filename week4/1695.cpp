f85526eb18f94974e8d2b7ffd11bbdd846ff57ae
sunnygeem <marguerite27@hanyang.ac.kr>
2023-01-25 16:42:14 +0900

    dp

--- week4/1695.cpp
+++ week4/1695.cpp
@@ -2,12 +2,13 @@
 
 #include <iostream>
 #include <algorithm>
+#include <string.h> //memset 포함
 
 using namespace std;
 
 int n;
 int arr[5001];
-int dp[2500][2500] = {-1, };
+int dp[5001][5001];
 
 int sol(int l, int r){ // right index, left index
     if(l >= r){
@@ -19,10 +20,10 @@ int sol(int l, int r){ // right index, left index
     }
 
     if(arr[l] == arr[r]){
-        dp[l][r] += sol(l+1, r-1);
+        dp[l][r] = sol(l+1, r-1);
     }
     else{
-        dp[l][r] += (1 + min(sol(l+1, r), sol(l, r-1)));
+        dp[l][r] = (1 + min(sol(l+1, r), sol(l, r-1)));
     }
 
     return dp[l][r];
@@ -34,6 +35,8 @@ int main(){
     for(int i=0; i<n; i++){
         cin>>arr[i];
     }
+
+    memset(dp, -1, sizeof(dp)); // 배열 초기화
 
     int ans = sol(0, n-1);
     cout<< ans << endl;
